4dacfbe428746de08d79b237f234f36123cbb5f1
Younes So <younes.creed.box@gmail.com>
2022-02-02 15:21:44 +0330

    seprate variables into a header file decreased the number of global variables

---
+++ variables.h
@@ -0,0 +1,33 @@
+#ifndef VARIABLES.H
+#define VARIABLES.H
+
+
+extern char lines[100];
+extern char world[20][20];
+extern int heaven_cords[100];
+extern int size;
+extern int Food_Position[100];
+extern int Food_energy[100];
+extern int NumberFood;
+extern int number_of_all;
+extern char winner;
+extern int sw;
+extern char direc;
+extern int nearest[2];
+extern int controlanimal;
+char world[20][20];
+int size;
+int Food_Position[100];
+int Food_energy[100];
+int NumberFood=0;
+int number_of_all=0;
+char winner;
+int sw = 0;
+char direc;
+int nearest[2];
+int controlanimal;
+
+
+
+
+#endif
